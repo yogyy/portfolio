b091e08f841746907581645195653df5a136f3d2
yogyy <yogimr261@gmail.com>
2023-03-14 08:40:56 +0700

    icon tai

--- src/components/darkTheme.tsx
+++ src/components/darkTheme.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 import { useTheme } from 'next-themes';
 import clsx from 'clsx';
-import { IoMoonOutline, IoSunnyOutline } from 'react-icons/io5';
 
 type ThemeButtonProps = React.ComponentPropsWithoutRef<'button'>;
 
@@ -23,61 +22,9 @@ export function ThemeButton({ className, ...rest }: ThemeButtonProps) {
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
       <MoonSun />
-      {theme === 'light' ? <IoMoonOutline /> : <IoSunnyOutline />}
     </button>
   );
 }
-
-const Moon = () => {
-  return (
-    <svg
-      stroke="currentColor"
-      fill="none"
-      strokeWidth="2"
-      viewBox="0 0 24 24"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      height="1em"
-      width="1em"
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      className="ionicon"
-      viewBox="0 0 512 512"
-    >
-      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-    </svg>
-  );
-};
-
-const Sun = () => {
-  return (
-    <svg
-      stroke="currentColor"
-      fill="none"
-      strokeWidth="2"
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 24 24"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      height="1em"
-      width="1em"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle cx="12" cy="12" r="5"></circle>
-      <line x1="12" y1="1" x2="12" y2="3"></line>
-      <line x1="12" y1="21" x2="12" y2="23"></line>
-      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-      <line x1="1" y1="12" x2="3" y2="12"></line>
-      <line x1="21" y1="12" x2="23" y2="12"></line>
-      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-    </svg>
-  );
-};
 
 const MoonSun = () => {
   return (
