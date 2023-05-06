fe8ffd6d2762225652a617a09dbe5f90da1d8304
yogyy <yogimr261@gmail.com>
2023-05-06 22:49:32 +0700

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/nprogess.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import Router from 'next/router';
+import { Analytics } from '@vercel/analytics/react';
 
 import nProgress from 'nprogress';
 import { PreloadProvider } from '@/components/PreloadContext';
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <PreloadProvider>
         <Component {...pageProps} />
+        <Analytics />
       </PreloadProvider>
     </ThemeProvider>
   );
