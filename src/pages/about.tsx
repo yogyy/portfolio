858dbb16af832d1f289112598bdcc5ba51e0a945
yogyy <yogimr261@gmail.com>
2023-06-05 14:52:16 +0700

    update about description

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,7 +13,6 @@ export default function AboutPage() {
       pageTitle="About | yogyy"
       content="yogyy is a Self-Tought Web Developer who based on West Java, Indonesia"
     >
-      <div className="pt-10"></div>
       <div className="flex flex-col justify-around h-full min-h-screen">
         <About className="mt-nav" />
         <IsLoaded>
@@ -29,6 +28,12 @@ export default function AboutPage() {
   );
 }
 
+const kebabCase = (string: string) =>
+  string
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/[\s_]+/g, '-')
+    .toLowerCase();
+
 export function About({ className, ...rest }: { className?: string }) {
   return (
     <>
@@ -42,37 +47,31 @@ export function About({ className, ...rest }: { className?: string }) {
 export function Me() {
   return (
     <div id="about" className="">
-      <h2 gaya-fade="1" className="text-2xl md:text-4xl 2xl:text-3xl">
-        <Accent>About</Accent>
+      <h2 gaya-fade="1" className="mb-6 text-2xl md:text-4xl 2xl:text-3xl">
+        <Accent>About Me</Accent>
       </h2>
-      <h1 gaya-fade="2" className="mt-1 text-2xl font-bold md:text-4xl 2xl:text-2xl">
-        me
-      </h1>
-      <div className="mt-4">
+      <div gaya-fade="2" className="mt-4">
         <Image
-          className="flex float-left w-40 mr-6 rounded md:w-52"
-          src={'https://res.cloudinary.com/dpegakmzh/image/upload/v1678442557/profile.jpg'}
+          className="flex float-left mr-6 bg-red-500 rounded-full w-28 md:w-36"
+          src="https://res.cloudinary.com/dpegakmzh/image/upload/c_scale,w_256/v1685623759/me_vwkosx.webp"
           alt="gwe"
           width={520}
           height={520}
-          draggable={false}
         />
       </div>
-      <article className="max-w-5xl mt-4 prose md:mt-6 dark:prose-invert">
-        <p gaya-fade="3" className="mb-4 leading-6">
-          Hello, my name is Yogi. &apos;yogyy&apos; is just a nickname I created. I&apos;m
+      <article className="max-w-4xl mt-4 md:mt-6">
+        <p gaya-fade="3" className="mb-4">
+          Hello, my name is YoGi. &apos;yogyy&apos; is just a nickname I created. I&apos;m
           interested in becoming a full-stack web developer. As a naturally curious person, I am
           always seeking ways to improve and achieve{' '}
           <span className="font-semibold text-red-500 line-through">perfection</span>
           &nbsp; better code in my work.They say practice makes perfect, but then again, they also
-          say that nobody&apos;s perfect, so I guess that means I don&apos;t have to practice~
+          say that nobody&apos;s perfect, so I guess that means I don&apos;t have to practice...
         </p>
-        <p gaya-fade="5" className="mb-4 leading-6">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut voluptas eius vitae, maxime
-          consequatur adipisci esse quibusdam dolorem asperiores iste qui nisi minus illo similique
-          quis nostrum sapiente laborum earum reprehenderit soluta suscipit. Dicta dolor reiciendis
-          ab dolores rem! Error beatae cupiditate molestias iure delectus ipsam. Consequuntur, sequi
-          praesentium! Culpa eius quas sapiente eum similique.
+        <p gaya-fade="4">
+          There are a lot of things and technologies to learn in frontend development and I am
+          motivated to learn as much as possible. I enjoy learning something new and getting
+          feedback to make myself better and improve.
         </p>
       </article>
     </div>
