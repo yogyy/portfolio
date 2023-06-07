6548e11dede7eab5b40f12da254eefb33b0865be
yogyy <yogimr261@gmail.com>
2023-06-07 19:25:38 +0700

    rename file

--- src/components/links/custom-link.tsx
+++ src/components/links/custom-link.tsx
@@ -1,17 +1,13 @@
 import clsx from 'clsx';
 import UnstyledLink, { UnstyledLinkProps } from './unstyledlink';
 
-export default function CustomLink({
-  children,
-  className = '',
-  ...rest
-}: UnstyledLinkProps) {
+export default function CustomLink({ children, className = '', ...rest }: UnstyledLinkProps) {
   return (
     <UnstyledLink
       {...rest}
       className={clsx(
         'animated-underline custom-link inline-flex items-center font-medium',
-        'focus:outline-none focus-visible:ring focus-visible:ring-dark-accent',
+        'focus:outline-none',
         'border-b border-dotted border-dark-accent hover:border-dark-secondary',
         className
       )}
