dc0f3337bd8f7274d5af32512db592d22bd7ae45
yogyy <yogimr261@gmail.com>
2023-08-30 21:14:05 +0700

    change name

--- src/lib/scrollToId.ts
+++ src/lib/scrollToId.ts
@@ -1,4 +1,4 @@
-export function scrollToSection(sectionId: string) {
+export default function scrollToSection(sectionId: string) {
   const section = document.getElementById(sectionId);
   section?.scrollIntoView({ behavior: 'smooth' });
 }
