b3759459a8730954b8fafcfc1a38465dc721b1b8
yogyy <yogimr261@gmail.com>
2023-09-16 01:52:20 +0700

    fix: aria label

--- src/components/layouts/spotify.tsx
+++ src/components/layouts/spotify.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import * as React from 'react';
 import { SiSpotify } from 'react-icons/si';
 import useSWR from 'swr';
-
+import { AiOutlineLoading } from 'react-icons/ai';
 import UnstyledLink, { UnstyledLinkProps } from '@/components/links/unstyledlink';
 import { SpotifyData, SpotifyLastPlayed } from '@/types/spotify';
 import axios from 'axios';
@@ -32,7 +32,11 @@ export default function Spotify({
   }, [data]);
 
   if (isLoading || lastPlay === null) {
-    return <div className="animate-pulse">loading ...</div>;
+    return (
+      <div className="sm:min-w-[233px] w-auto h-[70px] flex items-center justify-center">
+        <AiOutlineLoading className="text-2xl animate-spin text-dark-accent" />
+      </div>
+    );
   }
 
   if (data === undefined) {
@@ -40,9 +44,10 @@ export default function Spotify({
   }
 
   return data.isPlaying === true ? (
-    <figure id="spotify-activity" className={className}>
+    <figure aria-label="Spotify Activity" className={className}>
       <UnstyledLink
-        aria-label={`Spotify Now Playing: ${data.title} by ${data.artist}`}
+        // aria-label={`Spotify Now Playing: ${data.title} by ${data.artist}`}
+        aria-labelledby={`spotplay`}
         href={data.songUrl}
         className={clsx(
           'relative flex items-center gap-4 p-2.5',
@@ -61,10 +66,8 @@ export default function Spotify({
           width={240}
           height={240}
         />
-        <div className="flex-1">
-          <p className="text-xs font-medium">
-            <span className="hidden">spotify</span>Playing: {data.title}
-          </p>
+        <div className="flex-1" id="spotplay">
+          <p className="text-xs font-medium">Playing: {data.title} by</p>
           <p className="mt-1 text-xs font-semibold text-light-text/60 dark:text-dark-primary/50">
             {data.artist}
           </p>
@@ -75,10 +78,10 @@ export default function Spotify({
       </UnstyledLink>
     </figure>
   ) : (
-    <figure id="spotify-activity" className={className}>
+    <figure aria-label="Spotify Activity" className={className}>
       <UnstyledLink
-        {...rest}
-        aria-label={`Spotify Last Played: ${lastPlay?.title} by ${lastPlay?.artist}`}
+        // aria-label={`Spotify Last Played: ${lastPlay?.title} by ${lastPlay?.artist}`}
+        aria-labelledby="spotlast"
         href={lastPlay ? lastPlay?.songUrl : ''}
         className={clsx(
           'relative flex items-center gap-4 p-2.5',
@@ -88,6 +91,7 @@ export default function Spotify({
           'focus:outline-none focus-visible:ring focus-visible:ring-light-accent dark:focus-visible:ring-dark-accent',
           'bg-light-secondary/30 dark:bg-dark-bg/30'
         )}
+        {...rest}
       >
         {lastPlay && (
           <Image
@@ -98,9 +102,9 @@ export default function Spotify({
             height={240}
           />
         )}
-        <div className="flex-1 ">
-          <p className="text-xs font-medium flex">
-            <span className="hidden">spotify</span>Last Played: {lastPlay?.title}
+        <div className="flex-1" id="spotlast">
+          <p className="flex text-xs font-medium">
+            <span className="hidden">spotify</span>Last Played: {lastPlay?.title} by
           </p>
           <p className="mt-1 text-xs text-gray-600 dark:text-dark-primary/50">{lastPlay?.artist}</p>
         </div>
