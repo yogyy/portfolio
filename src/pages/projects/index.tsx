91869a3b3de76f57934d5952dd5957b824b0c9ce
yogyy <yogimr261@gmail.com>
2023-10-17 17:05:38 +0700

    fix: remove accordion, add more small projects

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,24 +1,26 @@
 import { Accent } from '@/components/accent';
 import IsLoaded from '@/components/isLoaded';
-import ProjectCardSimple from '@/components/layouts/ProjectCardSimple';
 import NextSEO from '@/components/Next-SEO';
 import ProjectCard from '@/components/project-card';
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@/components/ui/accordion';
-import clsx from 'clsx';
-import { allProjects } from 'contentlayer/generated';
-import React from 'react';
+import { cn } from '@/lib/utils';
 
-const smallProjects = [
+import { allProjects } from 'contentlayer/generated';
+import ProjectCardSimple from '@/components/project-card-simple';
+
+type ProjectType = {
+  title: string;
+  description: string;
+  url: string;
+  type: string[];
+  github?: string;
+}[];
+
+const smallProjects: ProjectType = [
   {
     title: 'yogyy',
     description: 'The website you are currently on',
     url: 'https://yogyy.vercel.app',
-    type: ['React', 'Typescript', 'NextJS', 'TailwindCSS', 'RadixUI'],
+    type: ['React', 'Typescript', 'NextJS', 'TailwindCSS', 'Radix UI'],
   },
   {
     title: 'Gallery',
@@ -41,58 +43,65 @@ const smallProjects = [
     url: 'https://yogyy.github.io/the-malaka',
     type: ['React', 'Typescript', 'TailwindCSS', 'Slicing'],
   },
+  {
+    title: 'Gh/Searcher',
+    description: "searching user & user's repo",
+    github: 'https://github.com/yogyy/ghsearcher',
+    url: 'https://yogyy.github.io/ghsearcher',
+    type: ['React', 'TypeScript', 'TailwindCSS', 'Radix UI'],
+  },
+  {
+    title: 'Burbir',
+    description: 'A twitter website clone',
+    github: 'https://github.com/yogyy/burungbiru',
+    url: 'https://burungbiru.vercel.app/',
+    type: ['NextJS', 'TypeScript', 'TailwindCSS', 'tRPC', 'Upstash', 'React Query'],
+  },
+  {
+    title: 'Spotify Activity',
+    description: 'Spotify Activity Card like in the footer',
+    url: '/spotify',
+    type: ['SWR'],
+  },
 ];
 
 const Projects = () => {
   return (
     <NextSEO
       className="layout"
-      title="Project Showcase - yogyy"
+      title="Project Showcase · Yogyy"
       desc="Showcase of my works on frontend development."
     >
-      <div className="h-full min-h-screen layout pt-14">
-        <IsLoaded>
-          <section className={clsx('py-20 h-full')}>
-            <article gaya-fade="0">
-              <h1 gaya-fade="2">
-                <Accent>Project i&apos;ve been working on so far</Accent>
-              </h1>
-              <h2
-                gaya-fade="3"
-                className="mt-2 text-gray-600 dark:text-gray-300 text-base font-medium"
-              >
-                Showcase of my works on frontend development.
-              </h2>
-              <ul gaya-fade="4" className="grid gap-4 mt-6 sm:grid-cols-2 xl:grid-cols-3">
-                {allProjects.map(project => (
-                  <ProjectCard key={project.slug} project={project} />
-                ))}
-              </ul>
-            </article>
-            <Accordion type="multiple" className="mt-4">
-              <AccordionItem value="item-1">
-                <AccordionTrigger className="px-2 rounded-md outline-none hover:no-underline focus-visible:outline-dark-accent">
-                  More project
-                </AccordionTrigger>
-                <AccordionContent>
-                  <ul className="grid gap-4 mt-6 sm:grid-cols-2 xl:grid-cols-3">
-                    {smallProjects.map(procj => (
-                      <ProjectCardSimple
-                        key={procj.title}
-                        description={procj.description}
-                        github={procj.github}
-                        title={procj.title}
-                        url={procj.url}
-                        type={procj.type}
-                      />
-                    ))}
-                  </ul>
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          </section>
-        </IsLoaded>
-      </div>
+      <IsLoaded>
+        <div className={cn('py-20')}>
+          <article gaya-fade="1">
+            <h1>
+              <Accent>Project i&apos;ve been working on so far</Accent>
+            </h1>
+            <h2 className="mt-2 text-base font-medium">
+              Showcase of my works on frontend development.
+            </h2>
+            <ul gaya-fade="2" className="grid gap-4 mt-6 sm:grid-cols-2 xl:grid-cols-3">
+              {allProjects.map(project => (
+                <ProjectCard key={project.slug} project={project} />
+              ))}
+            </ul>
+            <h3 className="mt-6">More Project</h3>
+            <ul gaya-fade="3" className="grid gap-4 mt-6 sm:grid-cols-2 xl:grid-cols-3">
+              {smallProjects.map(procj => (
+                <ProjectCardSimple
+                  key={procj.title}
+                  description={procj.description}
+                  github={procj.github}
+                  title={procj.title}
+                  url={procj.url}
+                  type={procj.type}
+                />
+              ))}
+            </ul>
+          </article>
+        </div>
+      </IsLoaded>
     </NextSEO>
   );
 };
