a53c625aa4a953f4ba2d0a7b1ec3cc7c3a1dd775
yogyy <yogimr261@gmail.com>
2023-12-14 01:56:35 +0700

    feat: add new analytic tool(posthog)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,16 +6,26 @@ import Router from 'next/router';
 
 import nProgress from 'nprogress';
 import { PreloadProvider } from '@/components/PreloadContext';
+import { PostHogProvider } from 'posthog-js/react';
+import posthog from 'posthog-js';
+import { isProd } from '@/constants/env';
 Router.events.on('routeChangeStart', nProgress.start);
 Router.events.on('routeChangeError', nProgress.remove);
 Router.events.on('routeChangeComplete', nProgress.done);
 
+if (typeof window !== 'undefined' && isProd) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+    api_host: 'https://app.posthog.com',
+  });
+}
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      <PreloadProvider>
-        <Component {...pageProps} />
-      </PreloadProvider>
-    </ThemeProvider>
+    <PostHogProvider client={posthog}>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+        <PreloadProvider>
+          <Component {...pageProps} />
+        </PreloadProvider>
+      </ThemeProvider>
+    </PostHogProvider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          async
-          src="https://analytics.eu.umami.is/script.js"
-          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID || ''}
-        ></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
