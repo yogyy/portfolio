e1fbeb61bb5e001da7155d503be270108c83630f
yogyy <yogimr261@gmail.com>
2024-02-21 21:12:34 +0700

    fix(font): add missing geist font on this page

--- src/pages/spotify.tsx
+++ src/pages/spotify.tsx
@@ -2,12 +2,20 @@ import SEO from 'next-seo.config';
 import { NextSeo } from 'next-seo';
 import { ThemeButton } from '@/components/dark-theme';
 import { Spotify } from '@/components/layouts';
+import { cn } from '@/lib/utils';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 
 const SpotifyPage = () => {
   return (
     <>
       <NextSeo title="Spotify Activity" {...SEO} />
-      <main className="relative grid min-h-screen w-screen place-content-center bg-gradient-to-br from-background to-accent">
+      <main
+        className={cn(
+          'relative grid min-h-screen w-screen place-content-center bg-gradient-to-br from-background to-accent',
+          `${GeistSans.variable} ${GeistMono.variable} font-sans`,
+        )}
+      >
         <Spotify className="border-none bg-background/25" />
         <ThemeButton className="absolute left-1/3 top-1/3 border-transparent bg-background/30" />
       </main>
