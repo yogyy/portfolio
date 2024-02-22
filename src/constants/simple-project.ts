dba4c41f75470431b6a539240e431c5e38a853d6
yogyy <yogimr261@gmail.com>
2024-02-23 01:23:16 +0700

    feat: update simple projects

---
+++ src/constants/simple-project.ts
@@ -0,0 +1,72 @@
+import { ProjectCardSimpleType } from '@/components/project-card-simple';
+
+const smallProjects: ProjectCardSimpleType[] = [
+  {
+    title: 'yogyy',
+    description: 'The website you are currently on',
+    url: 'https://yogyy.vercel.app',
+    type: ['React', 'Typescript', 'Next JS', 'Tailwind CSS', 'Radix UI'],
+  },
+  {
+    title: 'Gallery',
+    description: 'Searching Images using Unsplash API',
+    github: 'https://github.com/yogyy/Gallery',
+    url: 'https://gallery-yy.vercel.app',
+    type: ['React', 'Typescript', 'Next JS', 'Tailwind CSS'],
+  },
+  {
+    title: 'Indonesia Geography',
+    description: 'Showing Indonesia Geography and Searching Regency,Discrict, also Village',
+    github: 'https://github.com/yogyy/Indonesia-Geography',
+    url: 'https://nusantara-chi.vercel.app',
+    type: ['React', 'Typescript', 'Next JS', 'Tailwind CSS'],
+  },
+  {
+    title: 'The Malaka',
+    description: 'landing page for a book product.',
+    github: 'https://github.com/yogyy/the-malaka',
+    url: 'https://yogyy.github.io/the-malaka',
+    type: ['React', 'Typescript', 'Tailwind CSS', 'Slicing'],
+  },
+  {
+    title: 'Gh/Searcher',
+    description: "searching user & user's repo",
+    github: 'https://github.com/yogyy/ghsearcher',
+    url: 'https://yogyy.github.io/ghsearcher',
+    type: ['React', 'TypeScript', 'Tailwind CSS', 'Radix UI'],
+  },
+  {
+    title: 'DressLy',
+    description: 'landing page for fashion shop',
+    github: 'https://github.com/yogyy/DressLy',
+    url: 'https://yogyy.github.io/DressLy/',
+    type: ['Vue', 'TypeScript', 'Tailwind CSS'],
+  },
+  {
+    title: 'ToDo App',
+    description: 'Drag Drop To Do',
+    github: 'https://github.com/yogyy/react-todo-drag-and-drop',
+    url: 'https://yogyy.github.io/react-todo-drag-and-drop/',
+    type: ['React', 'TypeScript', 'Tailwind CSS'],
+  },
+  {
+    title: 'NestJS CRUD',
+    description: 'simple crud using nestjs',
+    github: 'https://github.com/yogyy/nest-crud',
+    type: ['Nest JS', 'TypeScript', 'MySql'],
+  },
+  {
+    title: 'Fastify CRUD',
+    description: 'simple crud using fastifyjs',
+    github: 'https://github.com/yogyy/nest-crud',
+    type: ['Fastify', 'JavaScript', 'MySql'],
+  },
+  {
+    title: 'Spotify Activity',
+    description: 'Spotify Activity Card like in the footer',
+    url: '/spotify',
+    type: ['SWR'],
+  },
+];
+
+export { smallProjects };
