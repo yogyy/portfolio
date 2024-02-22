026a147968e645c39fee014ffba1323d199b56a4
yogyy <yogimr261@gmail.com>
2024-02-22 18:33:07 +0700

    chore: add geist font to not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,19 @@
+import { cn } from '@/lib/utils';
 import { NextSeo } from 'next-seo';
 import Link from 'next/link';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 
 export default function Custom404() {
   return (
     <>
       <NextSeo title="404 not found" />
-      <div className="bg-dark-bg grid min-h-screen w-screen place-content-center bg-gradient-to-br">
+      <div
+        className={cn(
+          'bg-dark-bg grid min-h-screen w-screen place-content-center bg-gradient-to-br',
+          `${GeistSans.variable} ${GeistMono.variable} font-sans`,
+        )}
+      >
         <div className="relative flex h-screen flex-col items-center justify-center">
           <Link href="/" className="">
             <h1 className="bg-gradient-to-br from-yellow-500 to-red-600 bg-clip-text text-6xl text-transparent xl:text-9xl">
