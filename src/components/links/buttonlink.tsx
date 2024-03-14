1e7f3886bf98ba460f63e9072be6fe65c59a45cb
yogyy <yogimr261@gmail.com>
2024-03-15 03:08:52 +0700

    chore: remove unused css

--- src/components/links/buttonlink.tsx
+++ src/components/links/buttonlink.tsx
@@ -7,9 +7,10 @@ export default function ButtonLink({ children, className, ...props }: ButtonLink
   return (
     <UnstyledLink
       className={cn(
-        'animate-shadow inline-flex scale-100 rounded bg-background px-3 py-2 font-bold transition duration-100',
-        'hover:scale-[1.04] focus:scale-[1.03] focus:outline-none focus-visible:ring focus-visible:ring-primary active:scale-[0.97]',
-        'motion-safe:transform-gpu motion-reduce:hover:scale-100 motion-reduce:hover:brightness-90',
+        'inline-flex scale-100 rounded bg-background px-3 py-2 font-bold transition duration-100',
+        'after:absolute after:inset-0 after:z-[-1] after:opacity-0 after:shadow-md after:transition-opacity',
+        'hover:scale-[1.04] hover:after:opacity-100 focus:scale-[1.03] focus:outline-none focus-visible:ring focus-visible:ring-primary',
+        'active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 motion-reduce:hover:brightness-90 dark:after:shadow-none',
         className,
       )}
       {...props}
