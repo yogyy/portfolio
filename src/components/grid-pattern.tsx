18b96161cce08b23f52c76438d8121c12eca10bc
yogyy <yogimr261@gmail.com>
2024-04-15 20:30:16 +0700

    chore: reduce animatio

--- src/components/grid-pattern.tsx
+++ src/components/grid-pattern.tsx
@@ -79,15 +79,7 @@ export const GridPattern = ({ yOffset = 0, interactive = false, ...props }) => {
   }, [yOffset, interactive]);
 
   return (
-    <m.svg
-      ref={ref}
-      aria-hidden="true"
-      {...props}
-      exit={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      transition={{ ease: 'backInOut', duration: 0.5 }}
-    >
+    <svg ref={ref} aria-hidden="true" {...props}>
       <rect width="100%" height="100%" fill={`url(#${id})`} strokeWidth="0" />
       <svg x="50%" y={yOffset} strokeWidth="0" className="overflow-visible">
         {staticBlocks.map(block => (
@@ -119,6 +111,6 @@ export const GridPattern = ({ yOffset = 0, interactive = false, ...props }) => {
           <path d="M128 0 98.572 147.138A16 16 0 0 1 82.883 160H13.117a16 16 0 0 0-15.69 12.862l-26.855 134.276A16 16 0 0 1-45.117 320H-116M64-160 34.572-12.862A16 16 0 0 1 18.883 0h-69.766a16 16 0 0 0-15.69 12.862l-26.855 134.276A16 16 0 0 1-109.117 160H-180M192 160l-29.428 147.138A15.999 15.999 0 0 1 146.883 320H77.117a16 16 0 0 0-15.69 12.862L34.573 467.138A16 16 0 0 1 18.883 480H-52M-136 480h58.883a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1-18.883 320h69.766a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1 109.117 160H192M-72 640h58.883a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1 45.117 480h69.766a15.999 15.999 0 0 0 15.689-12.862l26.856-134.276A15.999 15.999 0 0 1 173.117 320H256M-200 320h58.883a15.999 15.999 0 0 0 15.689-12.862l26.856-134.276A16 16 0 0 1-82.883 160h69.766a16 16 0 0 0 15.69-12.862L29.427 12.862A16 16 0 0 1 45.117 0H128" />
         </pattern>
       </defs>
-    </m.svg>
+    </svg>
   );
 };
