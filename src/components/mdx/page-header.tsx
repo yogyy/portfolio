b8fa13ae593e430ed85a5fa4969951b629607e4c
yogyy <yogimr261@gmail.com>
2024-04-15 22:12:59 +0700

    feat: add animation
    add hr as optional

--- src/components/mdx/page-header.tsx
+++ src/components/mdx/page-header.tsx
@@ -1,18 +1,44 @@
+import { easeInoutQuad } from '@/constants/framer-easing';
 import { cn } from '@/lib/utils';
+import { m } from 'framer-motion';
+import { Accent } from '../accent';
 
 interface DocsPageHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
   heading: string;
   text?: string;
+  hr?: boolean;
 }
 
-export function DocsPageHeader({ heading, text, className, ...props }: DocsPageHeaderProps) {
+export function DocsPageHeader({
+  heading,
+  text,
+  hr = false,
+  className,
+  ...props
+}: DocsPageHeaderProps) {
   return (
     <>
       <div className={cn('space-y-4', className)} {...props}>
-        <h1 className="inline-block font-heading text-3xl lg:text-4xl">{heading}</h1>
-        {text && <p className="text-xl text-muted-foreground">{text}</p>}
+        <m.h1
+          initial={{ opacity: 0, x: -24 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ easings: easeInoutQuad, duration: 0.4 }}
+          className="font-heading inline-block text-3xl lg:text-4xl"
+        >
+          <Accent>{heading}</Accent>
+        </m.h1>
+        {text && (
+          <m.p
+            initial={{ opacity: 0, x: -26 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ easings: easeInoutQuad, duration: 0.4, delay: 0.1 }}
+            className="text-xl text-text/70"
+          >
+            {text}
+          </m.p>
+        )}
       </div>
-      <hr className="my-4" />
+      {hr && <hr className="my-4" />}
     </>
   );
 }
