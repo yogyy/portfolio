fb825920cf51f46683dd15069ec69b5cd89ef9fc
yogyy <yogimr261@gmail.com>
2024-11-19 22:07:20 +0700

    fix: reduce animation/motion

--- src/components/grid-pattern.tsx
+++ src/components/grid-pattern.tsx
@@ -1,6 +1,8 @@
 import { m } from 'framer-motion';
 import { useId, useRef } from 'react';
-import { useMediaQuery } from 'usehooks-ts';
+import { useIsClient } from '@/hooks/use-is-client';
+import { useMediaQuery } from '@/hooks/use-media-query';
+
 interface BlockProps extends Omit<React.ComponentPropsWithoutRef<typeof m.path>, 'x' | 'y'> {
   x: number;
   y: number;
@@ -8,8 +10,9 @@ interface BlockProps extends Omit<React.ComponentPropsWithoutRef<typeof m.path>,
 
 const Block = ({ x, y, ...props }: BlockProps) => {
   const notMobileScreen = useMediaQuery('(min-width: 768px)');
+  const client = useIsClient();
 
-  return notMobileScreen ? (
+  return client && notMobileScreen ? (
     <m.path
       animate={{ opacity: [0, 1, 0.25] }}
       transition={{ ease: 'easeIn', duration: 0.5 * x + y / 10 }}
@@ -34,12 +37,9 @@ export const GridPattern = ({ yOffset = 0, ...props }) => {
   ];
 
   return (
-    <m.svg
+    <svg
       ref={ref}
       aria-hidden="true"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
       className="fixed inset-x-0 -z-10 h-full w-full fill-primary/20 stroke-accent/5 md:[mask-image:linear-gradient(to_top_left,white_60%,transparent_60%)]"
       {...props}
     >
@@ -62,6 +62,6 @@ export const GridPattern = ({ yOffset = 0, ...props }) => {
           <path d="M128 0 98.572 147.138A16 16 0 0 1 82.883 160H13.117a16 16 0 0 0-15.69 12.862l-26.855 134.276A16 16 0 0 1-45.117 320H-116M64-160 34.572-12.862A16 16 0 0 1 18.883 0h-69.766a16 16 0 0 0-15.69 12.862l-26.855 134.276A16 16 0 0 1-109.117 160H-180M192 160l-29.428 147.138A15.999 15.999 0 0 1 146.883 320H77.117a16 16 0 0 0-15.69 12.862L34.573 467.138A16 16 0 0 1 18.883 480H-52M-136 480h58.883a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1-18.883 320h69.766a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1 109.117 160H192M-72 640h58.883a16 16 0 0 0 15.69-12.862l26.855-134.276A16 16 0 0 1 45.117 480h69.766a15.999 15.999 0 0 0 15.689-12.862l26.856-134.276A15.999 15.999 0 0 1 173.117 320H256M-200 320h58.883a15.999 15.999 0 0 0 15.689-12.862l26.856-134.276A16 16 0 0 1-82.883 160h69.766a16 16 0 0 0 15.69-12.862L29.427 12.862A16 16 0 0 1 45.117 0H128" />
         </pattern>
       </defs>
-    </m.svg>
+    </svg>
   );
 };
